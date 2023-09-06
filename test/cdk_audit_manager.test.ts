86cc59e98c0ea28b532f4ce04bb41e6047848879
bluestar712 <anatolii.blashkiv@outlook.com>
2023-09-06 03:29:48 -0400

    Initial commit

---
+++ test/cdk_audit_manager.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkAuditManager from '../lib/cdk_audit_manager-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_audit_manager-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkAuditManager.CdkAuditManagerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
